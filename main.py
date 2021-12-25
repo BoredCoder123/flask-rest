b54518ccd80a93f3476df253d1fab84580570e0f
ankitk2909 <ankitkathait28@gmail.com>
2021-12-26 00:13:22 +0530

    CRUD completed with db

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -19,38 +19,65 @@ class VideoModel(db.Model):
 
 # db.create_all() to be run only once
 
-# videos = {}
-
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Please send a name", required=True)
 video_put_args.add_argument("views", type=int, help="Please send a views", required=True)
 video_put_args.add_argument("likes", type=int, help="Please send a likes", required=True)
 
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Please send a name")
+video_update_args.add_argument("views", type=int, help="Please send a views")
+video_update_args.add_argument("likes", type=int, help="Please send a likes")
 
-def abort_if_video_id_doesnt_exits(video_id):
-    if video_id not in videos:
-        abort(404, message="video id is not valid...")
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
 
-def abort_if_video_id_exits(video_id):
-    if video_id in videos:
-        abort(409, message="video already exists with that id...")
 
 class Video(Resource):
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        abort_if_video_id_doesnt_exits(video_id)
-        return videos[video_id]
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="could not find video with that id")
+        return result, 200
 
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        # print(request.form['likes'])  this is the first method to get body but better to use reqparse
-
-        abort_if_video_id_exits(video_id)
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="video id already taken")
+        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
+
+
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="video doesn't exists, cannot update")
+        if args['name'] is not None:
+            result.name = args['name']
+        if args['views'] is not None:
+            result.views = args['views']
+        if args['likes'] is not None:
+            result.likes = args['likes']
+
+        db.session.commit()
+
+        return result
+
 
     def delete(self, video_id):
-        abort_if_video_id_doesnt_exits(video_id)
-        del videos[video_id]
+        VideoModel.query.filter_by(id=video_id).delete()
+        db.session.commit()
         return '', 204
 
 
--- test.py
+++ test.py
@@ -7,6 +7,11 @@ BASE = "http://127.0.0.1:5000/"
 #
 # print(requests.post(BASE+"hello-world/ankit").json())
 
-print(requests.put(BASE + "video/1", {"likes": 10, "name": "test", "views": 10}).json())
-print(requests.put(BASE + "video/1", {"likes": 10, "name": "test", "views": 10}).json())
-print(requests.delete(BASE + "video/1"))
+# print(requests.put(BASE + "video/3", {"likes": 10, "name": "test", "views": 10}).json())
+# print(requests.put(BASE + "video/4", {"likes": 10, "name": "test2", "views": 10}).json())
+# print(requests.get(BASE + "video/6").json())
+# print(requests.delete(BASE + "video/1"))
+print(requests.patch(BASE+"video/1", {"views": 99}).json())
+print(requests.get(BASE+"video/1").json())
+print(requests.delete(BASE+"video/1"))
+print(requests.get(BASE+"video/1").json)
