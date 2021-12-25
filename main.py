14005870f6a792650fb8acd4815928990877d725
ankitk2909 <ankitkathait28@gmail.com>
2021-12-25 23:42:19 +0530

    Moving to a sqlite database

--- main.py
+++ main.py
@@ -1,22 +1,60 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
-names = {"ankit": {"age": 25, "gender": "male"},
-         "charlie": {"age": 24, "gender": "male"}
-         }
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
 
-class HelloWorld(Resource):
-    def get(self, name):
-        return {"data": names[name]}
-
-    def post(self, name):
-        return {"data": {"name": "Post " + name}}
+    def __repr__(self):
+        return f"Video(name={self.name}, views={self.views}, likes={self.likes})"
 
 
-api.add_resource(HelloWorld, "/hello-world/<string:name>")
+# db.create_all() to be run only once
+
+# videos = {}
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Please send a name", required=True)
+video_put_args.add_argument("views", type=int, help="Please send a views", required=True)
+video_put_args.add_argument("likes", type=int, help="Please send a likes", required=True)
+
+
+def abort_if_video_id_doesnt_exits(video_id):
+    if video_id not in videos:
+        abort(404, message="video id is not valid...")
+
+def abort_if_video_id_exits(video_id):
+    if video_id in videos:
+        abort(409, message="video already exists with that id...")
+
+class Video(Resource):
+    def get(self, video_id):
+        abort_if_video_id_doesnt_exits(video_id)
+        return videos[video_id]
+
+    def put(self, video_id):
+        # print(request.form['likes'])  this is the first method to get body but better to use reqparse
+
+        abort_if_video_id_exits(video_id)
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_if_video_id_doesnt_exits(video_id)
+        del videos[video_id]
+        return '', 204
+
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,7 +2,11 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE+"hello-world/charlie")
-print(response.json())
+# response = requests.get(BASE+"hello-world/charlie")
+# print(response.json())
+#
+# print(requests.post(BASE+"hello-world/ankit").json())
 
-print(requests.post(BASE+"hello-world/ankit").json())
+print(requests.put(BASE + "video/1", {"likes": 10, "name": "test", "views": 10}).json())
+print(requests.put(BASE + "video/1", {"likes": 10, "name": "test", "views": 10}).json())
+print(requests.delete(BASE + "video/1"))
